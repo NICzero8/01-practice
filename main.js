65388a517cfcbd9c7273996d83f2c4fa80c9df67
niczero8 <niczero@gmail.com>
2023-10-17 21:28:06 +0300

    - various interctions added - mobile version added - code optimized

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.menu-button');
+const btnIcon = document.querySelector('.menu-lines');
+const mobileNav = document.querySelector('.mobile-menu');
+
+
+menuBtn.onclick = function () {
+    btnIcon.classList.toggle('open');
+    mobileNav.classList.toggle('open')
+}
