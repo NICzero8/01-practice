6e8ab84dacbdfda5f7590218655aaa5adc90fb55
niczero8 <niczero@gmail.com>
2023-10-18 17:28:30 +0300

    - fixed some issues

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const menuBtn = document.querySelector('.menu-button');
 const btnIcon = document.querySelector('.menu-lines');
 const mobileNav = document.querySelector('.mobile-menu');
 
-
 menuBtn.onclick = function () {
     btnIcon.classList.toggle('open');
-    mobileNav.classList.toggle('open')
-    document.body.classList.toggle('no-scroll')
+    mobileNav.classList.toggle('open');
+    document.body.classList.toggle('no-scroll');
 }
